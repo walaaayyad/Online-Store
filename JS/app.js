a0b40a8e4c8ab7cdfe852f7b9351eb68e6bbcfa7
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2021-01-15 09:59:15 +0200

    header

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+
+//Make slider vh resize to fit the screen
+// $(function (){
+//     'use strict';
+//     // Adjust Silder Height
+//     var winH   = $(window).height(),
+//         upperH = $('.upperBar').innerHeight(),
+//         navH   = $('.navbar').innerHeight();
+//     $('.silder').height(winH - (upperH + navH));
+//   });
+ /*
+ const fitSlider = ()=> {
+     let winH = window.outerHeight,
+         upperH = document.querySelector('.upperBar').clientHeight,
+         navH = document.querySelector('.navbar').clientHeight,
+         sliderH = document.querySelector('.slider').clientHeight;
+
+         console.log(sliderH);
+console.log(navH);
+console.log(upperH);
+console.log(winH);
+sliderH= winH-(upperH+navH);
+return;
+ };
+ fitSlider();*/
+
