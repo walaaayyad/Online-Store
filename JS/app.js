aaf008e96718d18264ee0a70b487f331ca39d301
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2021-02-02 11:12:32 +0200

    add fourth section

--- JS/app.js
+++ JS/app.js
@@ -24,3 +24,19 @@ return;
  };
  fitSlider();*/
 
+ $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
